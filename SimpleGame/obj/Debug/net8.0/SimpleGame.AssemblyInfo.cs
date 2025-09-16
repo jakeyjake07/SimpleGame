25100f291e8a9802d6e8710bb92601191e8229a8
jakeyjake07 <jaakob@live.se>
2025-09-16 07:41:21 +0200

    update

--- SimpleGame/obj/Debug/net8.0/SimpleGame.AssemblyInfo.cs
+++ SimpleGame/obj/Debug/net8.0/SimpleGame.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SimpleGame")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4a8c6650c720df5b8add755625400681561a065a")]
 [assembly: System.Reflection.AssemblyProductAttribute("SimpleGame")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SimpleGame")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
